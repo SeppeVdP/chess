44d7bf6ceeba5c77cfad490bc20871d7fe5635da
Seppe Van de Poel <seppevandepoel@hotmail.be>
2021-12-15 12:29:30 +0100

    added value to pieces

--- project/chess_utilities/example_utility.py
+++ project/chess_utilities/example_utility.py
@@ -1,26 +1,106 @@
 import chess
+import numpy as numpy
+
 from project.chess_utilities.utility import Utility
 
 class ExampleUtility(Utility):
 
     def __init__(self) -> None:
         pass
-    
+
+    PAWN_TABLE = numpy.array([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [5, 10, 10, -20, -20, 10, 10, 5],
+        [5, -5, -10, 0, 0, -10, -5, 5],
+        [0, 0, 0, 20, 20, 0, 0, 0],
+        [5, 5, 10, 25, 25, 10, 5, 5],
+        [10, 10, 20, 30, 30, 20, 10, 10],
+        [50, 50, 50, 50, 50, 50, 50, 50],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+
+    KNIGHT_TABLE = numpy.array([
+        [-50, -40, -30, -30, -30, -30, -40, -50],
+        [-40, -20, 0, 5, 5, 0, -20, -40],
+        [-30, 5, 10, 15, 15, 10, 5, -30],
+        [-30, 0, 15, 20, 20, 15, 0, -30],
+        [-30, 5, 15, 20, 20, 15, 0, -30],
+        [-30, 0, 10, 15, 15, 10, 0, -30],
+        [-40, -20, 0, 0, 0, 0, -20, -40],
+        [-50, -40, -30, -30, -30, -30, -40, -50]
+    ])
+
+    BISHOP_TABLE = numpy.array([
+        [-20, -10, -10, -10, -10, -10, -10, -20],
+        [-10, 5, 0, 0, 0, 0, 5, -10],
+        [-10, 10, 10, 10, 10, 10, 10, -10],
+        [-10, 0, 10, 10, 10, 10, 0, -10],
+        [-10, 5, 5, 10, 10, 5, 5, -10],
+        [-10, 0, 5, 10, 10, 5, 0, -10],
+        [-10, 0, 0, 0, 0, 0, 0, -10],
+        [-20, -10, -10, -10, -10, -10, -10, -20]
+    ])
+
+    ROOK_TABLE = numpy.array([
+        [0, 0, 0, 5, 5, 0, 0, 0],
+        [-5, 0, 0, 0, 0, 0, 0, -5],
+        [-5, 0, 0, 0, 0, 0, 0, -5],
+        [-5, 0, 0, 0, 0, 0, 0, -5],
+        [-5, 0, 0, 0, 0, 0, 0, -5],
+        [-5, 0, 0, 0, 0, 0, 0, -5],
+        [5, 10, 10, 10, 10, 10, 10, 5],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+
+    QUEEN_TABLE = numpy.array([
+        [-20, -10, -10, -5, -5, -10, -10, -20],
+        [-10, 0, 5, 0, 0, 0, 0, -10],
+        [-10, 5, 5, 5, 5, 5, 0, -10],
+        [0, 0, 5, 5, 5, 5, 0, -5],
+        [-5, 0, 5, 5, 5, 5, 0, -5],
+        [-10, 0, 5, 5, 5, 5, 0, -10],
+        [-10, 0, 0, 0, 0, 0, 0, -10],
+        [-20, -10, -10, -5, -5, -10, -10, -20]
+    ])
+    def get_piece_position_score(board: chess.Board, givenpiece: chess.Piece, table):
+        white = 0
+        black = 0
+        for x in range(1, 8):
+            for y in range(1, 8):
+                piece = board.piece_at(x*y)
+                if (piece != 0):
+                    if (piece.piece_type == givenpiece.piece_type):
+                        if (piece.color == givenpiece.color):
+                            white += table[x][y]
+                        else:
+                            black += table[7 - x][y]
+        return white - black
+
+
+
+
     # Calculate the amount of white pieces minus the amount of black pieces    
     def board_value(self, board: chess.Board):
+        # if winning move, take it
+        if board.is_checkmate():
+            return 999
+        position_score_queen_white = self.get_piece_position_score(givenpiece=chess.Piece(piece_type=chess.QUEEN, color=chess.WHITE), table=self.QUEEN_TABLE)
+        print(position_score_queen_white)
+        # give the board a value by amount of pieces and worth of each piece
         n_white = 0
         n_white += len(board.pieces(piece_type=chess.PAWN, color=chess.WHITE))
-        n_white += len(board.pieces(piece_type=chess.BISHOP, color=chess.WHITE))
-        n_white += len(board.pieces(piece_type=chess.KNIGHT, color=chess.WHITE))
-        n_white += len(board.pieces(piece_type=chess.ROOK, color=chess.WHITE))
-        n_white += len(board.pieces(piece_type=chess.QUEEN, color=chess.WHITE))
+        n_white += len(board.pieces(piece_type=chess.BISHOP, color=chess.WHITE)) * 3
+        n_white += len(board.pieces(piece_type=chess.KNIGHT, color=chess.WHITE)) * 3
+        n_white += len(board.pieces(piece_type=chess.ROOK, color=chess.WHITE)) * 5
+        n_white += len(board.pieces(piece_type=chess.QUEEN, color=chess.WHITE)) * 9
 
         n_black = 0
         n_black += len(board.pieces(piece_type=chess.PAWN, color=chess.BLACK))
-        n_black += len(board.pieces(piece_type=chess.BISHOP, color=chess.BLACK))
-        n_black += len(board.pieces(piece_type=chess.KNIGHT, color=chess.BLACK))
-        n_black += len(board.pieces(piece_type=chess.ROOK, color=chess.BLACK))
-        n_black += len(board.pieces(piece_type=chess.QUEEN, color=chess.BLACK))
-        return n_white - n_black
+        n_black += len(board.pieces(piece_type=chess.BISHOP, color=chess.BLACK)) * 3
+        n_black += len(board.pieces(piece_type=chess.KNIGHT, color=chess.BLACK)) * 3
+        n_black += len(board.pieces(piece_type=chess.ROOK, color=chess.BLACK)) * 5
+        n_black += len(board.pieces(piece_type=chess.QUEEN, color=chess.BLACK)) * 9
+        scoreAmountOfPieces = n_white - n_black
+        return scoreAmountOfPieces
 
 
--- project/chess_utilities/utility.py
+++ project/chess_utilities/utility.py
@@ -3,7 +3,28 @@ import chess
 
 """A generic utility class"""
 class Utility(ABC):
-    
+
     # Determine the value of the current board position (high is good for white, low is good for black, 0 is neutral)
     def board_value(self, board: chess.Board):
-        pass
+
+        #if winning move, take it
+        if chess.Outcome.winner:
+            return 999
+
+        #give the board a value by amount of pieces and worth of each piece
+        n_white = 0
+        n_white += len(board.pieces(piece_type=chess.PAWN, color=chess.WHITE))
+        n_white += len(board.pieces(piece_type=chess.BISHOP, color=chess.WHITE)) * 3
+        n_white += len(board.pieces(piece_type=chess.KNIGHT, color=chess.WHITE)) * 3
+        n_white += len(board.pieces(piece_type=chess.ROOK, color=chess.WHITE)) * 5
+        n_white += len(board.pieces(piece_type=chess.QUEEN, color=chess.WHITE)) * 9
+
+        n_black = 0
+        n_black += len(board.pieces(piece_type=chess.PAWN, color=chess.BLACK))
+        n_black += len(board.pieces(piece_type=chess.BISHOP, color=chess.BLACK)) * 3
+        n_black += len(board.pieces(piece_type=chess.KNIGHT, color=chess.BLACK)) * 3
+        n_black += len(board.pieces(piece_type=chess.ROOK, color=chess.BLACK)) * 5
+        n_black += len(board.pieces(piece_type=chess.QUEEN, color=chess.BLACK)) * 9
+        return n_white - n_black
+
+
